ee8ab8ad7a5e0d5cbfdca720d2cf9c4159b7f909
Hannah Ludema <hannahludema@Hannahs-MacBook-Air-4.local>
2016-09-30 17:04:13 -0400

    Initial commit

---
+++ app/src/main/java/edu/calvin/hel7/lab4/AboutActivity.java
@@ -0,0 +1,15 @@
+package edu.calvin.hel7.lab4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by hannahludema on 9/30/16.
+ */
+public class AboutActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
---
+++ app/src/main/java/edu/calvin/hel7/lab4/MainActivity.java
@@ -0,0 +1,33 @@
+package edu.calvin.hel7.lab4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_about:
+                startActivity( new Intent( getApplicationContext(), AboutActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
